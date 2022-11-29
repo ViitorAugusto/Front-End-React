3509c60384a813cc677d0fe8b7af39f007b37fd3
Vitor Augusto <vitoraugusto2010201078@gmail.com>
2022-11-29 20:03:33 -0300

    teste

--- MenuCardapio/src/pages/Cardapio/index.tsx
+++ MenuCardapio/src/pages/Cardapio/index.tsx
@@ -2,10 +2,16 @@ import styles from './Cardapio.module.scss';
 import  Logo  from '../../assets/logo.svg';
 export function Cardapio() {
   return (
-    <main>
+    <div>
         <nav className={styles.menu}>
             <img src={Logo} alt="" />
+            
         </nav>
-    </main>
+        <head className={styles.header}>
+          <div className={styles.header__text}>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore, reiciendis dolorem? Odit, quis aspernatur saepe eius amet beatae harum cumque. Asperiores nobis dolore culpa, voluptatum quod laboriosam et possimus ipsam!</p>
+          </div>
+        </head>
+    </div>
   );
 }
